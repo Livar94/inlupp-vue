5283e50518b43755e094a2f4a04b33bbab2c466a
livar94 <Hans.Mattin-Lassei@epndata.com>
2021-05-25 11:39:21 +0200

    init commit

---
+++ app/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="title">
+  <h1>Home Page</h1>
+
+ 
+
+
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.title{
+  padding: 5rem;
+}
+</style>
